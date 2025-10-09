f4c6af38dfd2751a4182582f2f5bcab4e298eea5
scontainer <scontainer@scontainer>
2025-10-10 00:34:05 +0800

    fix module not found error

--- datamodel/examples/3_coverage_of_a_template.py
+++ datamodel/examples/3_coverage_of_a_template.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 from unittest.mock import MagicMock, patch
 from datamodel.definitions.template import *
 
